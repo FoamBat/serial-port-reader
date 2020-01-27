88e684c86fcdcebdb259e4da0ec173b339cca89a
unknown <aivaras.goncarovas@gmail.com>
2020-01-27 16:23:04 +0200

    update

--- main.js
+++ main.js
@@ -5,27 +5,48 @@ const ByteLength = require('@serialport/parser-byte-length');
 const commands = require('./commands');
 
 const dataLabels = [
-  'Heat Sink Temperature',
-  'Panel 1 Voltage',
-  'Panel 1 DC Current',
-  'Working Hours',
-  'Working Hours',
+  'Heat Sink Temperature (C)',
+  0.1,
+  'Panel 1 Voltage (V)',
+  0.1,
+  'Panel 1 DC Current (A)',
+  0.1,
+  'Working Hours High Word',
+  1,
+  'Working Hours Low Word',
+  0.1,
   'Operating Mode',
-  'Tmp F',
-  'PV1 F',
-  'GFCI F',
+  1,
+  'Tmp F-Value (C)',
+  0.1,
+  'PV1 F-Value (V)',
+  0.1,
+  'GFCI F-Value (mA) ',
+  0.001,
   'Fault Code High',
+  1,
   'Fault Code Low',
-  'Line Current',
-  'Line Voltage',
-  'AC Frequency',
-  'AC Power',
-  'Zac',
-  'Accumulated Energy',
-  'Accumulated Energy',
-  'GFCI F',
-  'GFCI F',
-  'GZ F'
+  1,
+  'Line Current (A)',
+  0.1,
+  'Line Voltage (V)',
+  0.1,
+  'AC Frequency (Hz)',
+  0.01,
+  'AC Power (W)',
+  1,
+  'Zac (Ohms)',
+  0.001,
+  'Accumulated Energy High Word',
+  1,
+  'Accumulated Energy Low Word',
+  0.1,
+  'GFCI F-Value Volts (V)',
+  0.1,
+  'GFCI F-Value Hz (Hz)',
+  0.01,
+  'GZ F-Value Ohm (Ohms)',
+  0.001
 ];
 var serialNumberListener;
 
@@ -75,7 +96,9 @@ function dataReceived(data) {
   console.log(arr);
   for (let i = 0; i < 21; i++) {
     let data = (arr[9 + i * 2] << 8) + arr[10 + i * 2];
-    console.log(`${dataLabels[i]} - ${data}`);
+    console.log(
+      `${dataLabels[(i % 2) * 2]} - ${data * dataLabels[(i % 2) * 2 + 1]}`
+    );
   }
   clearInterval(serialNumberListener);
   /*setInterval(() => {
