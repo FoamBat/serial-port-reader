43bea3d084ac1fd2c60be066f94f598c84d4b1fc
unknown <aivaras.goncarovas@gmail.com>
2020-01-27 14:45:23 +0200

    update

--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 const inverterSerialNumber = [31, 30, 30, 30, 31, 38, 35, 31, 31, 30, 31]; // 11 Bytes
 
 function calculateChecksum(command) {
+  console.log(command + ' len = ' + command.length);
   const length = command.length;
   const checksum = command.reduce((acc, val) => {
     return acc + val;
