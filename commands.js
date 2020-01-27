891f673893221a8bc64ab1dbd9b01d623a3c6aaf
unknown <aivaras.goncarovas@gmail.com>
2020-01-27 10:55:25 +0200

    deleted:    codeFromC.js 	new file:   commands.js 	modified:   main.js

---
+++ commands.js
@@ -0,0 +1,25 @@
+const inverterSerialNumber = [49, 48, 48, 48, 50, 49, 50, 49, 49, 48, 49]; // 11 Bytes
+
+function calculateChecksum(command) {
+  const length = command.length;
+  const checksum = command.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+  console.log(checksum);
+  command[length] = checksum >> 8;
+  command[length + 1] = checksum & 255;
+
+  return checksum;
+}
+
+const commands = {
+  getSerialNumber: calculateChecksum([187, 187, 0, 0, 0, 0, 0, 0, 0]),
+  getConfigurations: calculateChecksum([187, 187, 1, 0, 0, 1, 1, 4, 0]),
+  getData: calculateChecksum([187, 187, 1, 0, 0, 1, 1, 2, 0]),
+  // format of LogIn command [9 bytes + 11 bytes of serial number + 2 bytes of checksum]
+  logIn: calculateChecksum(
+    [187, 187, 0, 0, 0, 0, 0, 1, 12].concat(inverterSerialNumber, 1)
+  )
+};
+
+module.exports = commands;
--- main.js
+++ main.js
@@ -1,65 +1,19 @@
 const SerialPort = require('serialport');
-//const Readline = require('@serialport/parser-readline');
-var Readline = SerialPort.parsers.Readline; // make instance of Readline parser
 
-// bb bb 00 00 00 00 00 80
-// 0b (11 bytes of data expected)
-// 31 30 30 30 31 38 35 31 31 30 31 04 23 ( 11 bytes + 2 bytes checksum)
-// bb bb 00 ff 00 00 00 00 01 15 02 8b
-//
+const commands = require('./commands');
+
 //let ss = '';
 //'0b 31 30 30 30 32 31 32 31 31 30 31'.split(' ').forEach((hexDigit) => {
 //  ss += parseInt(hexDigit, 16);
 //});
 
-// bb bb 00 00 00 00 00 80 (type of response. (80) Contains Inverter serial number in the Data field)
-// 0b (length of data = 11 in dec)
-// 31 30 30 30 32 31 32 31 31 30 31 (Serial Number [49, 48, 48, 48, 50, 49, 50, 49, 49, 48, 49])
-// 04 1a (last 2 bytes checksum)
-
-//case 1:	/* ask for serial number */
-// trame[0] = 0xbb; //187
-// trame[1] = 0xbb;
-// trame[2] = 0x00; //0
-// trame[3] = 0x00;
-// trame[4] = 0x00;
-// trame[5] = 0x00;
-// trame[6] = 0x00;
-// trame[7] = 0x00;
-// trame[8] = 0x00;
-// format of LogIn command [9 bytes + 11 bytes of serial number + 2 bytes of checksum]
-var inverter2SerialNumber = [49, 48, 48, 48, 49, 56, 53, 49, 49, 48, 49]; // 11 Bytes
-var inverter1SerialNumber = [49, 48, 48, 48, 50, 49, 50, 49, 49, 48, 49]; // 11 Bytes
-var logIn = [187, 187, 0, 0, 0, 0, 0, 1, 12]; // 9 Bytes
-var logInCommand = logIn.concat(inverter1SerialNumber);
-logInCommand[20] = 1;
-//calculateChecksum(logInCommand);
-console.log(`${logInCommand}  length = ${logInCommand.length}`);
-var askdata = [11, 187, 187, 1, 0, 0, 1, 1, 2, 0, 1, 123];
-
-var askForRegisters = [187, 187, 1, 0, 0, 1, 1, 2, 0];
-//calculateChecksum(askForRegisters);
-var trame = [187, 187, 0, 0, 0, 0, 0, 0, 0];
-var commandToGetConfigurations = [187, 187, 1, 0, 0, 1, 1, 4, 0];
-var pvOutputCom = [187, 187, 1, 0, 0, 1, 1, 2, 0];
-var commands = [trame];
-for (const command of commands) {
-  calculateChecksum(command);
-}
-function calculateChecksum(trame) {
-  const n = trame.length;
-  let checksum = 0;
-
-  for (let i = 0; i < n; i++) {
-    checksum += trame[i];
+class Inverter {
+  constructor(serialNumber) {
+    this.serialNumber = serialNumber;
   }
-  trame[n] = checksum >> 8;
-  trame[n + 1] = checksum & 255;
-
-  return checksum;
 }
 
-var port = new SerialPort(
+const port = new SerialPort(
   'COM1',
   {
     baudRate: 9600,
@@ -68,23 +22,20 @@ var port = new SerialPort(
     parity: 'none'
   },
   (error) => {
-    //console.log(`connection with serialport COM1 failed: ${error}`);
+    if (error) console.log(`connection with serialport COM1 failed: ${error}`);
   }
 );
 
 // sends data to the connected device via serial port
 function writeAndDrain(data) {
-  //console.log('Port is open = ', port.isOpen, ' isReadable = ', port.readable);
   console.log(data);
-  // flush data received but not read
+
   port.flush();
 
-  // write/send data to serial port
   port.write(data, function(error) {
     if (error) {
       console.log(error);
     } else {
-      // console.log(`drain and repeat write`);
       // waits until all output data has been transmitted to the serial port.
       port.drain(null);
     }
@@ -95,17 +46,10 @@ port.on('data', (data) => {
 });
 
 // The open event is always emitted
-port.on('open', function(res) {
+port.on('open', () => {
   console.log('Port open');
-  //writeAndDrain(commands[0]);
+  console.log(commands.getSerialNumber);
   setInterval(() => {
-    writeAndDrain(logInCommand);
+    writeAndDrain(commands.getSerialNumber);
   }, 1000);
 });
-
-/*setTimeout(() => {
-  if (!port.isOpen) return;
-  port.close(() => {
-    console.log('Port closed!');
-  });
-}, 120000);*/
