306d7ed9af888d8110066587f762f0d7198b6c6b
unknown <aivaras.goncarovas@gmail.com>
2020-01-27 15:17:46 +0200

    update

--- main.js
+++ main.js
@@ -59,6 +59,6 @@ parser.on('data', dataReceived);
 port.on('open', () => {
   console.log('Port open');
   serialNumberListener = setInterval(() => {
-    writeAndDrain(commands.getSerialNumber);
+    writeAndDrain(commands.getData);
   }, 1000);
 });
