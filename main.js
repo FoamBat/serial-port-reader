23fedc6d6b0d9b0bcce9372a1af2f0b7895455fb
unknown <aivaras.goncarovas@gmail.com>
2020-01-28 18:40:24 +0200

    update

--- main.js
+++ main.js
@@ -106,8 +106,6 @@ const dataLabels = [
   0.001
 ];
 
-var serialNumberListener;
-
 const port = new SerialPort(
   'COM1',
   {
@@ -120,9 +118,12 @@ const port = new SerialPort(
     if (error) console.log(`connection with serialport COM1 failed: ${error}`);
   }
 );
-const serialNumberParser = port.pipe(new ByteLength({ length: 11 }));
-const logInParser = port.pipe(new ByteLength({ length: 23 }));
-const dataParser = port.pipe(new ByteLength({ length: 53 }));
+let parser = port.pipe(new ByteLength({ length: 11 })); // Bytes in return. Data - 53 bytes, LogIn - 23 Bytes
+
+// listeners for receiving data
+var serialNumberListener;
+var logInListener;
+var dataListener;
 
 // sends data to the connected device via serial port
 function writeAndDrain(data) {
@@ -137,17 +138,6 @@ function writeAndDrain(data) {
     }
   });
 }
-function parseData() {
-  let arr = [...data];
-  let object = {};
-  object['Date Time'] = new Date();
-  for (let i = 0; i < 21; i++) {
-    let temp = (arr[9 + i * 2] << 8) + arr[10 + i * 2];
-    object[dataLabels[i * 2]] = temp * dataLabels[i * 2 + 1];
-    console.log(`${dataLabels[i * 2]} - ${data * dataLabels[i * 2 + 1]}`);
-  }
-  return object;
-}
 function appendDataToFile(data) {
   fs.appendFile(
     './data/data.json',
@@ -158,35 +148,68 @@ function appendDataToFile(data) {
     }
   );
 }
+function parseData(arr) {
+  let object = {};
+  object['Date Time'] = new Date();
+  for (let i = 0; i < 21; i++) {
+    let temp = (arr[9 + i * 2] << 8) + arr[10 + i * 2];
+    object[dataLabels[i * 2]] = temp * dataLabels[i * 2 + 1];
+    console.log(`${dataLabels[i * 2]} - ${data * dataLabels[i * 2 + 1]}`);
+  }
+  appendDataToFile(object);
+  return object;
+}
 function dataReceived(data) {
+  let hexByteDataArr = [...data];
+  dataLength = hexByteDataArr.length;
   console.log(data.length);
-  if (data.length === 11) {
-    console.log(`'Serial Number received' - ${data}`);
+  if (dataLength === 11) {
+    console.log(`Serial Number received - ${hexByteDataArr}`);
+    eventEmitter.emit('serial_number');
+  }
+  if (dataLength === 23) {
+    console.log(`Log In received - ${hexByteDataArr}`);
     eventEmitter.emit('log_in');
   }
-  if (data.length === 53) {
+  if (dataLength === 53) {
+    console.log(`Data From Inverter received - ${hexByteDataArr}`);
     eventEmitter.emit('data');
+    parseData(hexByteDataArr);
   }
 }
 
-logInParser.on('data', dataReceived);
+parser.on('data', dataReceived);
 
 // The open event is always emitted
 port.on('open', () => {
   console.log('Port open = ', port.isOpen);
-  dataListener = setInterval(() => {
+
+  serialPortListener = setInterval(() => {
     writeAndDrain(commands.getSerialNumber);
   }, 3000);
 
-  // data event received, check when was the last time data was received from inverter
-  eventEmitter.on('data', function() {
-    console.log('data read event fired!');
+  eventEmitter.on('serial_port', function() {
+    clearInterval(serialPortListener);
+    parser = port.pipe(new ByteLength({ length: 23 }));
+
+    logInListener = setInterval(() => {
+      writeAndDrain(commands.logIn);
+    }, 3000);
   });
+
   // log in to inverter received, start asking data from inverter
   eventEmitter.on('log_in', function() {
-    console.log('data read event fired!');
+    clearInterval(logInListener);
+    parser = port.pipe(new ByteLength({ length: 53 }));
+
     dataListener = setInterval(() => {
       writeAndDrain(commands.getData);
     }, 3000);
+  });
+
+  // data event received, check when was the last time data was received from inverter
+  eventEmitter.on('data', function() {
+    dataReadTimestamp = new Date().toLocaleString();
+    console.log('data read event fired!');
   });
 });
