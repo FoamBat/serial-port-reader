ad6ffe7c24a8a6de811bb3a1c4610869013698a2
unknown <aivaras.goncarovas@gmail.com>
2020-02-04 18:00:08 +0200

    commit

--- main.js
+++ main.js
@@ -45,7 +45,9 @@ function initNewCommunication(port) {
 }
 function onOpen() {
   let port = this;
-  console.log(`${new Date().toLocaleString()} ${port.name} Port opened.`);
+  console.log(
+    `${new Date().toLocaleString()} ${JSON.stringify(port)} Port opened.`
+  );
   const parser = constructByteLengthParser(RETURN_BYTES_OF_SERIAL);
   let Communicator = new SerialCommunicator(port, parser);
 
