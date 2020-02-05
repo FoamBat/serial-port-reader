e106f9bd26c9e1d29a5b010aeb697f211150c44e
unknown <aivaras.goncarovas@gmail.com>
2020-02-05 10:45:17 +0200

    commit

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   username: process.env.SN_USERNAME,
   password: process.env.SN_PASSWORD,
   instanceUrl: process.env.SN_INSTANCE_URL,
-  DATA_INTERVAL: 1000 * 30, // 30 seconds
-  LOGIN_INTERVAL: 1000 * 3, // 3 seconds
+  DATA_INTERVAL: 1000 * 60, // 60 seconds
+  LOGIN_INTERVAL: 1000 * 10, // 10 seconds
   RETURN_BYTES_OF_SERIAL: 22,
   RETURN_BYTES_OF_LOGIN: 12,
   RETURN_BYTES_OF_DATA: 53
